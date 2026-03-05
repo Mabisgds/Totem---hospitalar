2255e1b6fc8212e3389ac574a48aa9aeb37e2e2e
Gustavo Prates <daviherick8@gmail.com>
2026-03-05 20:05:52 -0300

    Update paciente.py

--- models/paciente.py
+++ models/paciente.py
@@ -1,1 +1,21 @@
+class Paciente:
+    def __init__(self, nome, idade, pcd):
+        self.nome = nome
+        self.idade = idade
+        self.pcd = pcd
 
+    def ficha(self):
+        return {
+            "nome": self.nome,
+            "idade": self.idade,
+            "pcd": self.pcd
+        }
+    
+    def cadastro(self):
+        print("Insira seu nome: ")
+        self.nome = input()
+        print("Insira sua idade: ")
+        self.idade = int(input())
+        print("possui deficiência? (s/n): ")
+        pcd_input = input().lower()
+        self.pcd = True if pcd_input == "s" else False
