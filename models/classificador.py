3306033160e1d1fbf156459b02133dee046fd837
Gustavo Prates <daviherick8@gmail.com>
2026-03-05 21:06:17 -0300

    Update classificador.py

--- models/classificador.py
+++ models/classificador.py
@@ -1,1 +1,16 @@
 
+from paciente import Pacientes
+
+class Classificador:
+    def __init__(self, ficha):
+        self.ficha = ficha
+
+    def classificar(self):
+        if self.ficha["pcd"] == "s" or self.ficha["idade"] >= 60:
+            return "Prioridade Alta"
+
+        elif 45 <= self.ficha["idade"] < 60:
+            return "Prioridade Média"
+
+        else:
+            return "Prioridade Baixa"
