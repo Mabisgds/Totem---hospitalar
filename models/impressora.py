e1a490e13b495cf22a5c40b9855f8fea6784a911
asRamoos <0001140501@senaimgaluno.com.br>
2026-03-06 22:44:38 -0300

    Update impressora.py

--- models/impressora.py
+++ models/impressora.py
@@ -1,1 +1,25 @@
+from datetime import datetime
 
+class Impressora:
+
+    def imprimir_ticket(self, paciente, tipo, servico, senha):
+
+        print("\n------------------------------------------")
+        print("TICKET DE ATENDIMENTO")
+        print("------------------------------------------")
+
+        print(f"PACIENTE: {paciente.nome}")
+
+        if tipo == "PRIORITARIO":
+            print("TIPO: PRIORITÁRIO (Lei 10.048)")
+        else:
+            print("TIPO: Geral")
+
+        print(f"SERVIÇO: {servico}")
+        print(f"SENHA: {senha}")
+
+        data = datetime.now().strftime("%d/%m/%Y %H:%M")
+        print(f"DATA: {data}")
+
+        print("------------------------------------------")
+        print("Aguarde ser chamado no painel central.")
