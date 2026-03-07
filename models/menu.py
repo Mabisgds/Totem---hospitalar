b581090c5d48598087bc49e30054f41d22be2ba7
M. Gabriela <mariagabrielasdg@gmail.com>
2026-03-06 23:18:54 -0300

    menu aplicado
    Nao estava clicavel o botao do merge

--- models/menu.py
+++ models/menu.py
@@ -1,1 +1,32 @@
+class Menu:
+    def __init__(self):
+        self.executando = True
 
+    def mostrar_menu(self):
+        print("\nMENU PRINCIPAL")
+        print("Escolha uma opção")
+        print("1 - Consulta")
+        print("2 - Exame")
+        print("3 - Emergência")
+        print("0 - Sair")
+
+    def executar(self):
+        while self.executando:
+            self.mostrar_menu()
+            opcao = input("Escolha: ")
+
+            if opcao == "1":
+                self.consulta()
+            elif opcao == "2":
+                self.exame()
+            elif opcao == "3":
+                self.emergencia()
+            elif opcao == "0":
+                print("Saindo...")
+                self.executando = False
+            else:
+                print("Opção inválida")
+
+
+menu = Menu()
+menu.executar()
