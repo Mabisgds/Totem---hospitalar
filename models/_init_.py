2b7361ce75f1c4830e4e250d3fc900665ca0f6a5
asRamoos <0001140501@senaimgaluno.com.br>
2026-03-06 21:09:31 -0300

    Update _init_.py

--- models/_init_.py
+++ models/_init_.py
@@ -1,1 +1,11 @@
 
+class Menu:
+    def exibir_menu(self):
+        print("\n[MENU DE SERVIÇOS]")
+        print("1. Consulta")
+        print("2. Exame")
+        print("3. Emergência")
+
+        opcao = input("Escolha a opção: ")
+
+        return opcao
