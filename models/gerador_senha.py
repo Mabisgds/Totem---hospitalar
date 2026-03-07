ac4ed5b4a7a9f363e91de05f935bf61857856862
asRamoos <0001140501@senaimgaluno.com.br>
2026-03-06 21:08:40 -0300

    TICKET

--- models/gerador_senha.py
+++ models/gerador_senha.py
@@ -1,1 +1,14 @@
+class GeradorSenha:
 
+    contador_geral = 0
+    contador_prioritario = 0
+
+    def gerar(self, tipo):
+
+        if tipo == "PRIORITARIO":
+            GeradorSenha.contador_prioritario += 1
+            return f"P-{GeradorSenha.contador_prioritario:03}"
+
+        else:
+            GeradorSenha.contador_geral += 1
+            return f"G-{GeradorSenha.contador_geral:03}"
