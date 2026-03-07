d3ce64c313a574be125eb70aec5bd5bb2c55b395
M. Gabriela <mariagabrielasdg@gmail.com>
2026-03-06 23:13:54 -0300

    Arquivo corrigido

--- models/paciente.py
+++ models/paciente.py
@@ -1,13 +1,8 @@
-<<<<<<< Updated upstream
 class Pacientes:
-=======
-class Paciente:
->>>>>>> Stashed changes
     def __init__(self, nome, idade, pcd):
         self.nome = nome
         self.idade = idade
         self.pcd = pcd
-<<<<<<< Updated upstream
 
     def ficha(self):
         return {
@@ -24,21 +19,4 @@ class Paciente:
         print("possui deficiência? (s/n): ")
         self.pcd = input().lower()
         
-=======
->>>>>>> Stashed changes
 
-    def ficha(self):
-        return {
-            "nome": self.nome,
-            "idade": self.idade,
-            "pcd": self.pcd
-        }
-    
-    def cadastro(self):
-        print("Insira seu nome: ")
-        self.nome = input()
-        print("Insira sua idade: ")
-        self.idade = int(input())
-        print("possui deficiência? (s/n): ")
-        pcd_input = input().lower()
-        self.pcd = True if pcd_input == "s" else False
